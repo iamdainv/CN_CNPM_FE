03cbe6bf4381779cb9d8a9f7ac27f399a7ef7775
iamdainv <dai.nguyen2@sotatek.com>
2021-09-10 17:00:51 +0700

    add env firebase config

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,19 +1,18 @@
 import firebase from 'firebase'
-
+import { envConfig } from './env.config'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBy83QtIcQKvzXktb4AwWr51rGpObQ6-dw',
-  authDomain: 'shobbe-a305f.firebaseapp.com',
-  projectId: 'shobbe-a305f',
-  storageBucket: 'shobbe-a305f.appspot.com',
-  messagingSenderId: '320273411730',
-  appId: '1:320273411730:web:dd7b9009e96ea491e0404b',
-  measurementId: 'G-RVTSFWCDPD'
+  apiKey: envConfig.firebaseApiKey,
+  authDomain: envConfig.firebaseAuthDomain,
+  projectId: envConfig.firebaseProjectId,
+  storageBucket: envConfig.firebaseStorageBucket,
+  messagingSenderId: envConfig.firebaseMessagingSenderId,
+  appId: envConfig.firebaseAppId,
+  measurementId: envConfig.firebaseMeasurementId
 }
-console.log('firebase', firebase)
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
 
